9ce810f5743753d94c58b8525911c5e547c79708
Robert Stinnett <robert@robertstinnett.com>
2019-02-17 09:05:11 -0600

    v1.0 Release

--- README.md
+++ README.md
@@ -1,41 +1,27 @@
 ## Synopsis
 
-**syncCurrency** is a currency exchange door originally designed for running under [Synchronet BBS Software](http://www.synchro.net).  It will display the current
-exchange rates for $1.00USD in various currencies.  It uses data from the Free API at [Open Exchange Rates](https://openexchangerates.org).  It was originally written by Robert Stinnett aka dmxrob of [Gateway to the West BBS - bbs.dmxrob.net](telnet://bbs.dmxrob.net) as a way to get back into BBSing and learn more about Synchronet and Javascript.
+**syncSMS** is a SMS/text messaging communications door originally designed for running under [Synchronet BBS Software](http://www.synchro.net).  It allows users to 
+send text messages to the SysOp as a method of communicating with him/her without having to disclose the SysOp's cell number. It requires an API key from [Twilio](https://twilio.com). It was originally written by Robert Stinnett aka dmxrob of [Gateway to the West BBS - bbs.dmxrob.net](telnet://bbs.dmxrob.net) as a way to get back into BBSing and learn more about Synchronet and Javascript.
+
+The SysOp receives the message showing the username who sent it, the phone number the user provided and the message.
+
 
 ## Screenshots 
 
 Regular View:  
-![syncCurrency Main Screen Shot](http://blog.dmxrob.net/wp-content/uploads/2019/02/syncCurrency-Main-Menu-1.11.png)
-
-![syncCurrency Search for Specific Currency Screen Shot](http://blog.dmxrob.net/wp-content/uploads/2019/02/syncCurrency-Search-Function-1.11.png)
+![syncSMS Main Screen Shot](http://blog.dmxrob.net/wp-content/uploads/2019/02/syncSMS-1.0.png)
 
 ## Installation
 
-Check out [sysop.txt](https://github.com/robertstinnett/syncCurrency/blob/master/sysop.txt) for full installation instructions.
+Check out [sysop.txt](https://github.com/robertstinnett/syncSMS/blob/master/sysop.txt) for full installation instructions.
 
 ## License
 
 This project is under GNU License.
-Please see the [LICENSE](https://github.com/robertstinnett/syncCurrency/blob/master/LICENSE) file for the project.
+Please see the [LICENSE](https://github.com/robertstinnett/syncSMS/blob/master/LICENSE) file for the project.
 
 ## Revision History (change log)
 
-1.00 (2019-02-10)
+1.00 (2019-02-17)
 * First  release, here's hoping for the best!
 
-1.10 (2019-02-12)
-* Added ability to search for specific currencies
-* Added ability to display all known currencies
-* Changed API endpoints to variables
-* Changed base currency amount to variable
-* Fixed typos in documentation
-
-1.11 (2019-02-12)
-* Fixed typo in sysop.txt for configuration
-* Added second screenshot to README.md file
-* Added color prompts to menu items
-
-1.20 (2019-XX-XX)
-* Added routine to check for automatic updates
-* Added 
--- sms.js
+++ sms.js
@@ -1,4 +1,4 @@
-//syncSMS by dmxrob - http://blog.dmxrob.net | ssh://bbs.dmxrob.net | telnet://bbs.dmxrob
+//syncSMS by dmxrob (Robert Stinnett) - http://blog.dmxrob.net | ssh://bbs.dmxrob.net | telnet://bbs.dmxrob
 //Github:  https://github.com/robertstinnett/syncSMS
 //Code for Error Handling by Kirkman - http://breakintochat.com & https://github.com/Kirkman
 
@@ -15,20 +15,10 @@ var drkcy = "\001n\001c"; //Synchronet Ctrl-A Code for Dark (normal) Cyan
 var cy = "\001c\1h"; //Synchronet Ctrl-A Code for High Intensity Cyan
 var gr = "\001g\1h"; //Synchronet Ctrl-A Code for High Intensity Green
 
+// Set some program variables
 
-
-var version = cy + "1.00" + bl + " - " + cy + "XX/XX/2019";
-
-var apiEndpoint = "https://api.twilio.com/2010-04-01/Accounts/";
-var apiMessages = "latest.json?app_id=";
-var apiCurrencyListCall = "currencies.json?prettyprint=true&show_alternative=false&show_inactive=false";  // Freebie call!
-
-var updateURL = "http://api.dmxrob.net/doorcheck/"
-var updateFlag = true;
-
-var usd_amount = 1;  // Base currency amount in USD
-
-log(user.ip_address);
+var version = cy + "1.00" + bl + " - " + cy + "02/17/2019";
+var apiEndpoint = "https://api.twilio.com/2010-04-01/Accounts/"; // Twilo Endpoint
 
 //Load modopts.ini info early so we can detect if the section exists for [syncSMS]
 //see sysop.txt for what should be in this section
@@ -44,14 +34,10 @@ load("sbbsdefs.js"); //loads a bunch-o-stuff that is probably beyond the underst
 
 // Get configuration information
 // See the sysop.txt file for more information
-var smsNumber = opts.smsNumber;
+var smsFromNumber = opts.smsFromNumber;
+var smsToNumber = opts.smsToNumber;
 var twilioAccountSid = opts.twilioAccountSid;
 var twilioAuthToken = opts.twilioAuthToken;
-var updateFlag = opts.updateFlag;
-
-
-
-
 
 
 function displayHeader() {
@@ -81,153 +67,62 @@ function displayHeader() {
 
 function sendSMSMessage() {
 	// Get the message to send and then send it!
-	var mySMSMessage = "";
-
-	console.putmsg(bl + "Enter your message below. Limit of 160 characters. Keep it short and sweet.");
+	
+	console.putmsg(bl + "Enter your phone number: ");
+	var myPhone =  console.getstr(LEN_PHONE,K_UPPER|K_LINE|K_EDIT|K_AUTODEL);
+	console.crlf();
+	console.putmsg(bl + "Enter your message below. Limit of 120 characters. Keep it short and sweet.");
 	console.crlf();
 	console.putmsg(yl + ":");
-	mySMSMessage = console.getstr(79,K_WRAP);
-	mySMSMessage = mySMSMessage + console.getstr(79);
+	var mySMSMessage = console.getstr(79,K_WRAP|K_LINE|K_EDIT);
+	console.putmsg(yl + ":" + wh);
+	mySMSMessage = mySMSMessage + console.getstr(41,K_LINE|K_EDIT);
 
 	if (console.yesno("Send above text msg to " + system.operator + "")) {
 		// Ok, let's send it.
 
-		var req = new HTTPRequest();
-		var reqData = "From=+13142074299To=" + smsNumber + "Body=" + mySMSMessage;
-		console.putmsg("DEBUG:  reqData = " + reqData);console.crlf(2);
-		console.putmsg("DEBUG:  reqData Encoded = " + encodeURI(reqData));console.crlf(2);
-		var sendSMSResult = req.post(apiEndpoint + twilioAccountSid  + "/Messages.json",reqData);
+		mySMSMessage = "U:" + user.alias + " P:" + myPhone + " M:" + mySMSMessage;
+
+		log("syncSMS Message Sent: "+ mySMSMessage);
+
+		var req = new HTTPRequest(twilioAccountSid,twilioAuthToken);
+		var reqData = "From=" + smsFromNumber + "&To=" + smsToNumber + "&Body=" + mySMSMessage;
+		var sendSMSResult = req.Post(apiEndpoint + twilioAccountSid  + "/Messages.json",encodeURI(reqData));
+
 		if (sendSMSResult === undefined) {
 			// Something went wrong
-			log("ERROR in sms.js:  Request to openexchangerates.org returned nothing/undefined");
-			console.putmsg("We're sorry, Currency Exchange Rates are not available right now. :-(")
+			log("ERROR in sms.js (syncSMS):  Request to Twilio returned nothing/undefined");
+			console.putmsg("Sorry, something went wrong - SysOp notified. :-(")
+			console.pause();
 			exit();
-		}
-		console.putmsg("DEBUG:  result = " + sendSMSResult);
-
-		// Parse the JSON
-		var currentRatesJSON = JSON.parse(currentRatesResponse);
-
-	}
-}
-
-function checkForUpdates(updateFlag) {
-	// This function checks, if enabled, the current version and alerts if a new version is available.
-	// See sysop.txt for information on enabling/disabling
-
-
-	if (updateFlag) {
-
-	}
-}
-
-
-function getExchangeRates() {
-// This function calls the API and displays the current exchange rates. 
-
-		
-		// Make the API call
-		var req = new HTTPRequest();
-		var currentRatesResponse = req.Post(apiEndpoint +  + openExchangeAPIkey);
-		if (currentRatesResponse === undefined) {
-			// Something went wrong
-			log("ERROR in sms.js:  Request to openexchangerates.org returned nothing/undefined");
-			console.putmsg("We're sorry, Currency Exchange Rates are not available right now. :-(")
-			exit();
-		}
-
-		// Parse the JSON
-		var currentRatesJSON = JSON.parse(currentRatesResponse);
-
-
-
-		// Print it out
-		var exchangeRates = Object.keys(currentRatesJSON.rates);
-		var dt = new Date(currentRatesJSON.timestamp*1000);
-		console.crlf();
-		console.putmsg(bl + "Exchange Rates Current as of " + yl + dt);
-		console.crlf();
-		console.putmsg(bl + "Rates shown below are for " + wh + usd_amount + " USD.")
-		console.crlf();
-		console.crlf();
-	
-		
-		for (var i = 0; i < exchangeRates.length; i++) {
-			if (currencyTypeList.indexOf(exchangeRates[i]) > -1) {
-				console.putmsg(currentRatesJSON.rates[exchangeRates[i]] + " - " + exchangeRates[i]);
-				console.crlf();
-			}
-		}
-
-		
-	
-	
-		console.crlf();
-
-		// Return JSON object with rates so we don't have to continue making repeat calls to API 
-
-		return currentRatesJSON; 
-		
-		
-
-
-
-}
-
-
-function searchExchangeRates(currentRatesJSON) {
-	// This function will allow the user to search Exchange Rates for a currency.
-
-	var exchangeRates = Object.keys(currentRatesJSON.rates);
-	var ready_to_exit = false;
-	var user_input = "";
-	
-	var dt = new Date(currentRatesJSON.timestamp*1000);
-
-	while (!ready_to_exit && bbs.online) {
-		console.putmsg(bl + "Enter currency to search for, " + wh + "?" + bl + " for list or " + wh + "Q" + bl + " to quit: ")
-		user_input = console.getstr(user_input,3);  // Read a maximum of 3 letters in.
-		
-		if (user_input == "Q" || user_input == "q") {
-			ready_to_exit = true;
-		}
-		else if (user_input == "?") { // Display all the currencies to choose from
-			console.putmsg(rd + "Valid currencies to choose from:");
-			console.crlf();
-			for (var i = 0; i < exchangeRates.length; i++) { // Print out currencies, 15 per line.
-					if (i%15 == 0) {
-						console.crlf();
-					}
-					console.putmsg(wh + exchangeRates[i] + gy + ",");
-					
-				
-			}
-			console.crlf();
-
-
-		}
-		else if (exchangeRates.indexOf(user_input) != -1) { //Found it, display it
-			console.crlf();
-			console.putmsg(wh + usd_amount + cy + " USD" + bl + " is equal to " + wh + currentRatesJSON.rates[user_input] + cy + " " + user_input + bl + " as of " + yl + dt);
-			console.crlf(2);
 		}
 		else {
-			console.putmsg(rd + "Invalid choice " + wh + user_input + rd + ". Try again.");
-			console.crlf();
-		}
+			console.crlf(2);
+			var twilioResponseJSON = JSON.parse(sendSMSResult);
+			if (twilioResponseJSON.status == "queued") {
+		
+				console.putmsg(bl + "Your text message has been sent to the SysOp.");
+				console.crlf();
+			}
+			else {
+				console.putmsg(rd + "Error sending messaging.  Aborted.");
+				log("ERROR in sms.js (syncSMS): Message failed to send. Twilio status returned " + twilioResponseJSON.status);
+				console.crlf();
+
+			}
+
+		}		
+	}
+	else {
+		console.putmsg(rd + "Message send aborted.  Nothing sent.");
+		console.crlf();
+	}
+}
 
 		
-
-	}
-
-}
-       
-			
-
 try {
 
-	// Print a welcome message and header
-
+	log("syncSMS launched: " + user.alias + " - " + user.ip_address);
 	displayHeader();
 	sendSMSMessage();
 	console.pause();
@@ -237,9 +132,7 @@ try {
 } catch (err) {
 
 log("ERROR in sms.js. " + err);
-log(LOG_DEBUG,"DEBUG for sms.js. API call looked like this at time of error: " + apiEndpoint + openExchangeAPIkey);
 log(LOG_DEBUG,"DEBUG for sms.js. The user.connection object looked like this at the time of error: " + user.connection);
-
 
 } finally {
 
